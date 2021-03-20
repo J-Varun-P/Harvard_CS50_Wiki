b63e6d44dd21a66c35602547c3c5c33a16aa7cfd
Varun <varunpawarjetling@gmail.com>
2021-03-20 08:50:48 +0530

    Testing again since I made a video last night, I'm trying my best, still have a lot to learn though

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -31,9 +31,7 @@ def title(request, title):
         s2 = s1.splitlines()
         s3 = markdown2.markdown(s1)
         s4 = s3.splitlines()
-        #form = Title(initial={'pagecontent': s1})
         form = Textarea(initial={'title': title, 'pagecontent': s1})
-        #form = Title(initial={'pagecontent': s1})
         return render(request, "encyclopedia/title.html", {
         "content": s4, "title": title, "pagecontent": s2, "form": form
         })
@@ -41,22 +39,17 @@ def title(request, title):
 
 def addpage(request):
     if request.method == "POST":
-        #print("Hello")
         form = Title(request.POST)
         abcd = form.is_valid()
         if form.is_valid():
-            #print("I'm Here, look at me!!!!!!!!!!!!!!!!")
             title = form.cleaned_data["title"]
             pagecontent = form.cleaned_data["pagecontent"]
-            #print(title, util.get_entry(title))
             if util.get_entry(title) != None:
                 return render(request, "encyclopedia/newtitlefail.html")
             else:
                 util.save_entry(title,pagecontent)
                 return HttpResponseRedirect(f"{title}")
-                #return render(request, "encyclopedia/index.html", {
-                #    "entries": util.list_entries()
-                #})
+
     else:
         return render(request, "encyclopedia/addpage.html")
 
@@ -65,18 +58,6 @@ def randompage(request):
     entries = util.list_entries()
     n = random.randint(0, len(entries) - 1)
     return HttpResponseRedirect(f"{entries[n]}")
-    """
-    print(f"{entries[n]}")
-    s1 = util.get_entry(entries[n])
-    s2 = s1.splitlines()
-    s3 = markdown2.markdown(s1)
-    s4 = s3.splitlines()
-    return HttpResponseRedirect(f"{entries[n]}")
-    return render(request, "encyclopedia/title.html", {
-    "content": s4
-    })
-    return HttpResponse(f"{entries[n]}")
-    """
 
 
 def editpage(request):
@@ -87,7 +68,6 @@ def editpage(request):
         return render(request, "encyclopedia/edittitle.html", {
         "form": formedit, "title": title, "pagecontent": pagecontent
         })
-        #return HttpResponse("I hope this works")
 
 
 def search(request):
@@ -96,8 +76,6 @@ def search(request):
     if formsearch.is_valid():
         q = formsearch.cleaned_data["q"]
     r = util.list_entries()
-    #print(q)
-    #print(r)
     if q in r:
         return HttpResponseRedirect(f"{q}")
     else:
@@ -114,11 +92,9 @@ def search(request):
 
 def editpagesave(request):
     if request.method == "POST":
-        #return HttpResponse(f"Please work")
         form_edited_save_it = Textarea(request.POST)
         if form_edited_save_it.is_valid():
             title = form_edited_save_it.cleaned_data["title"]
-            #return HttpResponseRedirect(f"{title}")
             pagecontent = form_edited_save_it.cleaned_data["pagecontent"]
             util.save_entry(title, pagecontent)
             return HttpResponseRedirect(f"{title}")
---
+++ entries/Testing 5.md
@@ -0,0 +1,7 @@
+#### This is for testing purposes
+
+- 1
+- 2
+- 3
+- 4
+- 5
